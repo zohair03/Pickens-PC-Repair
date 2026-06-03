bd5207ee215939bfd6b0ca377aab7a90b65c1e9b
Zohair Ahmed <zohairrahmed03@gmail.com>
2026-06-03 19:44:10 +0530

    menu bar color fixed - changed to white

--- components/sections/navbarSection/topBar.jsx
+++ components/sections/navbarSection/topBar.jsx
@@ -71,9 +71,9 @@ const TopBar = ({ menuOpen, toggleMenu }) => {
           onClick={toggleMenu}
           aria-label={menuOpen ? "Close menu" : "Open menu"}
         >
-          <div className={`w-6 h-0.5 bg-black transition-all ${menuOpen ? "rotate-45 translate-y-1.5" : ""}`} />
-          <div className={`w-6 h-0.5 bg-black transition-all ${menuOpen ? "opacity-0" : ""}`} />
-          <div className={`w-6 h-0.5 bg-black transition-all ${menuOpen ? "-rotate-45 -translate-y-1.5" : ""}`} />
+          <div className={`w-6 h-0.5 bg-topbar-menuicon transition-all ${menuOpen ? "rotate-45 translate-y-1.5" : ""}`} />
+          <div className={`w-6 h-0.5 bg-topbar-menuicon transition-all ${menuOpen ? "opacity-0" : ""}`} />
+          <div className={`w-6 h-0.5 bg-topbar-menuicon transition-all ${menuOpen ? "-rotate-45 -translate-y-1.5" : ""}`} />
         </button>
       </div>
     </div>
